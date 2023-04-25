c937edccaf205a153326815adfd6bc73cf3ee6c4
Bartosz Lewandowski <bartosz.m.lewandowski@gmail.com>
2023-04-25 12:57:51 +0000

    Upload New File

---
+++ Attribute_data_operations__chap_3_.R
@@ -0,0 +1,25 @@
+library(sf)      # vector data package introduced in Chapter 2
+library(terra)   # raster data package introduced in Chapter 2
+library(dplyr)   # tidyverse package for data frame manipulation
+library(spData)  # spatial data package introduced in Chapter 2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
